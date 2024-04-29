8c58ae232b6776f931f6dc1480ec09fb1a9ac17d
ChristopherDesrosiersMondor <desrosch@gmail.com>
2024-04-29 18:34:04 -0400

    server runs

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,3 +1,28 @@
-export async function bootstrap(): Promise<string> {
-  return "bootstrapped 👋"
+import 'dotenv/config'
+import 'reflect-metadata'
+
+import express from 'express'
+import { Container } from 'inversify'
+import { InversifyExpressServer } from 'inversify-express-utils'
+
+import './webLayer/equipment.controller'
+
+console.clear()
+
+export async function bootstrap() {
+  const container = new Container()
+  const server = new InversifyExpressServer(container)
+
+  server.setConfig((app) => {
+    app.use(express.json())
+  })
+
+  const app = server.build()
+
+  app.listen(process.env.PORT, () => {
+    console.log(`server is running on http:localhost:${process.env.PORT}`)
+  })
+  return {}
 }
+
+bootstrap()
